e524b4d8242f7eea2de3aeec0662a0d9689361f8
Kim Tae Sung <kaoss12@naver.com>
2025-10-19 14:46:00 +0900

    Left&Right Rotate 함수 구현

--- ts/src/rbtree.c
+++ ts/src/rbtree.c
@@ -22,4 +22,47 @@ rbtree* new_rbtree (void)
   p->root = p -> nil;
   
   return p;
+ 
+}
+
+void Left_Rotate (rbtree* t, node_t* node)
+{
+  node_t* y = node -> right;
+  node -> right = y -> left;
+
+  if(y -> left != t -> nil)
+  y -> left -> parent = node;
+
+  y -> parent = node -> parent;
+
+  if(node -> parent == t -> nil)
+  t -> root = y;
+  else if(node == node -> parent -> left)
+  node -> parent -> left = y;
+  else
+  node -> parent -> right = y;
+
+  y -> left = node;
+  node -> parent = y;
+}
+
+void Right_Rotate (rbtree* t, node_t* node)
+{
+  node_t* y = node -> left;
+  node -> left = y -> right;
+
+  if(y -> right != t -> nil)
+  y -> right -> parent = node;
+
+  y -> parent = node -> parent;
+
+  if(node -> parent == t -> nil)
+  t -> root = y;
+  else if(node == node -> parent -> right)
+  node -> parent -> right = y;
+  else
+  node -> parent -> left = y;
+
+  y -> right = node;
+  node -> parent = y;
 }
